15b515e3f325eba330a7de8e3930c185ce52f871
Martin Barker <martinbarker99@gmail.com>
2024-07-05 10:54:14 -0700

    db route async promise

--- server.js
+++ server.js
@@ -8,10 +8,19 @@ console.log('server.js staring')
 // Enable CORS for all routes
 app.use(cors());
 
-app.get('/dbtest', (req, res) => {
-  res.send('Hello from the Node.js server!');
+async function makeTestQuery(){
+  return new Promise(async function (resolve, reject) {
+    resolve({"cool":"stuff"})
+  })
+}
+
+app.get('/dbtest', async (req, res) => {
+  console.log("Making test query")
+  let queryResults = await makeTestQuery()
+  res.send(queryResults);
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
