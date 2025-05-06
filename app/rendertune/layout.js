d9187f955fa244702837c245f652724758f6d4f9
unknown <martinbarker99@gmail.com>
2025-05-05 18:11:56 -0700

    imgs added, 404 fix err

--- app/rendertune/layout.js
+++ app/rendertune/layout.js
@@ -3,7 +3,7 @@
 export default function RenderTuneLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ border: '5px solid red', padding: '10px' }}>
+      <body>
         <h1>RenderTune Layout Top</h1>
         {children}
         <h1>RenderTune Layout Bottom</h1>
---
+++ app/rendertune/not-found.js
@@ -0,0 +1,7 @@
+export default function RenderTuneNotFound() {
+    return (
+      <div>
+        <h1>404 – RenderTune</h1>
+      </div>
+    );
+  }
