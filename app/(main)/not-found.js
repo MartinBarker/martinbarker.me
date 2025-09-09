15707bfc04fefad7e7d6c41a9589ead70d071dd0
unknown <martinbarker99@gmail.com>
2025-09-08 19:15:09 -0700

    main 404 added

--- app/(main)/not-found.js
+++ app/(main)/not-found.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function NotFound() {
   return (
     <div>
-      <h2>(main) 404</h2>
+      <h2>aaaaaaaaaaaaaaaa</h2>
       <Link href="/">Return Home</Link>
     </div>
   )
